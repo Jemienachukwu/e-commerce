05eab2e0643235e3b864149a42ba9f694e0b1452
Udosinachi <79016579+Jemienachukwu@users.noreply.github.com>
2022-07-15 20:33:21 +0100

    first commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './index.css'
+import { Container } from 'react-bootstrap'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+
 const App = () => {
   return (
-    <>
-      <h1>hiii</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
   )
 }
 

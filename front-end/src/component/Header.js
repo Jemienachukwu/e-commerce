103bf6d4c31ff0c967856c2a03f79bad96973863
Udosinachi <79016579+Jemienachukwu@users.noreply.github.com>
2022-07-19 22:13:36 +0100

    sync backend data

--- front-end/src/component/Header.js
+++ front-end/src/component/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="m-auto">
+            <Nav className="mr-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>Cart

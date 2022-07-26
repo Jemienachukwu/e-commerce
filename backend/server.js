bd62784e4bc22defa4903c652d5f1eecd4de0f7d
Udosinachi <79016579+Jemienachukwu@users.noreply.github.com>
2022-07-26 17:28:22 +0100

    installed mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import express from'express'
-import dotenv from'dotenv'
-import products from'./data/products.js'
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 dotenv.config()
 const app = express()
 
